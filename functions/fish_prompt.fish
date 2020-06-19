a017776bc1c95996ab8d76ff732724ce3a6f3c3d
Joe Fiorini <jfiorini@wayfair.com>
2020-06-19 10:25:16 -0400

    Add nix-shell support

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,3 +1,10 @@
+function nix_shell_info
+  if test -n "$IN_NIX_SHELL"
+    echo -n "<nix-shell> "
+  end
+end
+
+
 function cmd_status
   set -l code $status
   set_color green
@@ -20,5 +27,5 @@ function jobs_count
 end
 
 function fish_prompt
-  echo (cmd_status) (jobs_count) (set_color normal)
+  echo (nix_shell_info) (cmd_status) (jobs_count) (set_color normal)
 end
